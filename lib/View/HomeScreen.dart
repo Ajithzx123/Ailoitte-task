190738aea104e33d1e1af11ffab66fac7a943e89
Ajithzx123 <kumarajith577@gmail.com>
2022-07-27 12:43:28 +0530

    code added

--- lib/View/HomeScreen.dart
+++ lib/View/HomeScreen.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+const  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,58 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: PreferredSize(
           child: Obx(
-            () => appBar(controller: controller),
+            () => AppBar(
+              backgroundColor: Colors.white,
+              actions: [
+                controller.issearch == true
+                    ? Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                controller.searchApi();
+                              },
+                              icon: const Icon(
+                                Icons.search,
+                                color: Colors.black,
+                              )),
+                          IconButton(
+                              onPressed: () {
+                                controller.issearch.value = false;
+                              },
+                              icon: const Icon(
+                                Icons.close,
+                                color: Colors.black,
+                              )),
+                        ],
+                      )
+                    : IconButton(
+                        onPressed: () {
+                          controller.issearch.value = true;
+                        },
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ))
+              ],
+              title: controller.issearch == true
+                  ? TextField(
+                      onChanged: ((value) {
+                        controller.searchInput.value = value;
+                      }
+                  ),
+                  
+                  textInputAction: TextInputAction.done,
+                    )
+                  : Text(
+                      "Drinks",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.sp),
+                    ),
+              centerTitle: true,
+              toolbarHeight: 13.h,
+            ),
           ),
           preferredSize: Size.fromHeight(8.h)),
       body: Obx(() => controller.searchList.isEmpty
@@ -27,8 +78,7 @@ class HomeScreen extends StatelessWidget {
                       color: const Color.fromARGB(255, 227, 227, 227),
                       borderRadius: BorderRadius.circular(5.w)),
                   child: InkWell(
-                    onTap: () => Get.to(
-                        DetailScreen(item: controller.searchList[index])),
+                    onTap: () => Get.to(DetailScreen(item: controller.searchList[index])),
                     child: ListTile(
                       leading: CircleAvatar(
                           backgroundImage: NetworkImage(
@@ -36,77 +86,13 @@ class HomeScreen extends StatelessWidget {
                       )),
                       title: Text(
                         controller.searchList[index].name.toString(),
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      subtitle:
-                          Text("${controller.searchList[index].category}"),
+                      subtitle: Text("${controller.searchList[index].category}"),
                     ),
                   ),
                 );
               })),
-    );
-  }
-}
-
-class appBar extends StatelessWidget {
-  const appBar({
-    Key? key,
-    required this.controller,
-  }) : super(key: key);
-
-  final ListController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.white,
-      actions: [
-        controller.issearch == true
-            ? Row(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        controller.searchApi();
-                      },
-                      icon: Icon(
-                        Icons.search,
-                        color: Colors.black,
-                      )),
-                  IconButton(
-                      onPressed: () {
-                        controller.issearch.value = false;
-                      },
-                      icon: Icon(
-                        Icons.close,
-                        color: Colors.black,
-                      )),
-                ],
-              )
-            : IconButton(
-                onPressed: () {
-                  controller.issearch.value = true;
-                },
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ))
-      ],
-      title: controller.issearch == true
-          ? TextField(
-              onChanged: ((value) {
-                controller.searchInput.value = value;
-              }),
-              textInputAction: TextInputAction.done,
-            )
-          : Text(
-              "Drinks",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.sp),
-            ),
-      centerTitle: true,
-      toolbarHeight: 13.h,
     );
   }
 }
--- lib/service/http_Services.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ailoitte/Model/dataModel.dart';
-import 'package:dio/dio.dart';
-
-
-
